939a48b680e4a5e88c6e7ec878c361fa0b78918e
А.Максимов <mai@it-expertise.ru>
2025-03-07 14:57:43 +0300

    add reading from client

---
+++ main.go
@@ -0,0 +1,39 @@
+package redis
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
+
+func main() {
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		buf := make([]byte, 1024)
+
+		n, err := conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("error reading from client:", err.Error())
+			os.Exit(1)
+		}
+		fmt.Println(string(buf[:n]))
+
+		conn.Write([]byte("+OK\r\n"))
+	}
+}
